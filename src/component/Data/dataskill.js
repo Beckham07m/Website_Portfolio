3be12c9782dc6b6532107f6db7056471c3944c39
Sitthichai <zerobox584boxzazaza@gmail.com>
2024-01-19 00:27:00 +0700

    chage file data

---
+++ src/component/Data/dataskill.js
@@ -0,0 +1,28 @@
+import html from "../../assets/pictures/html.png";
+import css from "../../assets/pictures/css.png";
+// import javascript from "../assets/pictures/javascript.png";
+// import bootstrap from "../assets/pictures/bootstrap.png";
+// import mui from "../assets/pictures/mui.png";
+import react from "../../assets/pictures/react.png";
+// import tailwind from "../assets/pictures/tailwind.png";
+
+export const fend = [
+  {
+    id: 1,
+    src: html,
+    title: "HTML",
+    style: "shadow-orange-500",
+  },
+  {
+    id: 2,
+    src: css,
+    title: "CSS",
+    style: "shadow-blue-500",
+  },
+  {
+    id: 3,
+    src: react,
+    title: "React",
+    style: "shadow-blue-600",
+  },
+];
